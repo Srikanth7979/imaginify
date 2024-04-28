3e3747ce288bda063c139efb1c856afd96ab08f2
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2024-04-28 19:00:55 +0300

    - Integrate Clerk within the Application

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,21 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="root">
       <div className="container">
-        <div className="wrapper">{children}</div>
+        <div className="wrapper">
+          <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </div>
       </div>
     </main>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const plexSans = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -21,10 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", plexSans.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", plexSans.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
